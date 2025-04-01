bbc3b7cecc9029805bb06353729d3a4e4feaf146
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-04-01 20:13:24 +0200

    Graph QL Tests

--- apollo-server/index.js
+++ apollo-server/index.js
@@ -1,5 +1,7 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
+const { GraphQLError } = require('graphql')
 
 let persons = [
   {
@@ -25,25 +27,92 @@ let persons = [
 ]
 
 const typeDefs = `
+
+type Address{
+street: String!
+city: String!
+}
+
 type Person {
 name: String!
+address: Address!
 phone: String
-street: String!
-city: String
 id: ID!
+}
+
+enum YesNo {
+YES
+NO
 }
 
 type Query {
 personCount: Int!
-allPersons: [Person!]!
+allPersons(phone: YesNo): [Person!]!
 findPerson(name: String!): Person
 }
+
+type Mutation{
+
+changeNumber(
+name: String!
+phone: String!
+): Person
+
+
+addPerson(
+name: String!
+phone: String
+street: String!
+city: String!
+): Person
+}
 `
+
+const throwError = (message, code, invalidArgs) => {
+  throw new GraphQLError(message, {
+    extensions: {
+      code,
+      invalidArgs,
+    },
+  })
+}
+
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
-    findPerson: (root, args) => persons.find(p => p.name === args.name),
+    allPersons: (root, args) => {
+      return !args.phone
+        ? persons
+        : persons.filter((p) => (args.phone === 'YES' ? p.phone : !p.phone))
+    },
+    findPerson: (root, args) => persons.find((p) => p.name === args.name),
+  },
+  Person: {
+    address: (root) => ({ city: root.city, street: root.street }),
+  },
+  Mutation: {
+    addPerson: (root, args) => {
+      if (persons.find((p) => p.name === args.name)) {
+        throw new GraphQLError('Name must be unique', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+          },
+        })
+      }
+      persons = persons.concat({ ...args, id: uuid() })
+      return persons[persons.length - 1]
+    },
+    changeNumber: (root, args) => {
+      const person = persons.find((p) => p.name === args.name)
+      if (!person) {
+        throwError('User name cannot be found', 'SERVER_ERROR', args.name)
+      }
+      const editedPerson = { ...person, phone: args.phone }
+
+      persons = persons.map((p, i) => (p.name === args.name ? editedPerson : p))
+      return editedPerson
+    },
   },
 }
 
