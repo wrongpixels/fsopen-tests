f60cf2d19ab01c5409b8b1c284b26dc4454f8ba4
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-04-07 19:20:02 +0200

    Apollo Server as middleware

--- apollo-server/phonebook-backend/index.js
+++ apollo-server/phonebook-backend/index.js
@@ -1,6 +1,14 @@
 const { ApolloServer } = require('@apollo/server')
 const jwt = require('jsonwebtoken')
-const { startStandaloneServer } = require('@apollo/server/standalone')
+const { expressMiddleware } = require('@apollo/server/express4')
+const {
+  ApolloServerPluginDrainHttpServer,
+} = require('@apollo/server/plugin/drainHttpServer')
+const { makeExecutableSchema } = require('@graphql-tools/schema')
+const express = require('express')
+const cors = require('cors')
+const http = require('http')
+
 const config = require('./utils/config')
 const mongoose = require('mongoose')
 const User = require('./models/user')
@@ -15,22 +23,36 @@ mongoose
   .then(() => console.log('Connected to Mongoose'))
   .catch((e) => console.log('error', e.message))
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
+const start = async () => {
+  const app = express()
+  const httpServer = http.createServer(app)
 
-startStandaloneServer(server, {
-  listen: { port: config.PORT },
-  context: async ({ req }) => {
-    const decodedToken = req?.headers?.authorization?.startsWith('Bearer ')
-      ? jwt.verify(req.headers.authorization.substring(7), config.SECRET)
-      : null
-    const currentUser = decodedToken
-      ? await User.findById(decodedToken.id).populate('friends')
-      : null
-    return { currentUser }
-  },
-}).then(({ url }) => {
-  console.log(`server ready at ${url}`)
-})
+  const server = new ApolloServer({
+    schema: makeExecutableSchema({ typeDefs, resolvers }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  })
+
+  await server.start()
+  app.use(cors())
+  app.use(express.json())
+  app.use(
+    '/',
+    expressMiddleware(server, {
+      context: async ({ req }) => {
+        const decodedToken = req?.headers?.authorization?.includes('Bearer ')
+          ? jwt.verify(req.headers.authorization.substring(7), config.SECRET)
+          : null
+        if (decodedToken) {
+          const currentUser = await User.findById(decodedToken.id).populate(
+            'friends',
+          )
+          return { currentUser }
+        }
+      },
+    }),
+  )
+  httpServer.listen(config.PORT, () =>
+    console.log(`Server is really running on http://localhost:${config.PORT}`),
+  )
+}
+start()
