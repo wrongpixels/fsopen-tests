77d6c579668525366497fb3ec2e5d9ea4f15f38f
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-05-07 17:58:53 +0200

    Added postgres notes basic api

---
+++ notes-postgres/index.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const {Sequelize, QueryTypes, Model, DataTypes, json} = require('sequelize')
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+class Note extends Model {}
+Note.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    content:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    important:{
+        type: DataTypes.BOOLEAN
+    },
+    date: {
+        type: DataTypes.DATE
+    }
+}, {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'note'
+})
+
+app.get('/api/notes', async (_, res) => {
+    try {
+        const notes = await Note.findAll()
+        res.json(notes)
+
+    } catch (error){
+        res.status(500).json({error: error.message})
+    }
+})
+
+app.post ('/api/notes', async (req, res) => {
+    try{
+        const note = req.body
+        console.log(note)
+        const newNote = await Note.create(note)
+        res.json(newNote)
+    }catch(e){
+        console.log(e.message)
+        res.status(400).json({error: e.message})
+    }
+})
+const port = process.env.PORT || 3001
+app.listen(port, () => {console.log(`Server running on port: ${port}`)})
