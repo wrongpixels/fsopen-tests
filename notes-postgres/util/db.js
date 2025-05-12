a5aa0c2bd595408902cbb8685f68a7019d760fc6
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-05-12 20:03:45 +0200

    Roll down added

--- notes-postgres/util/db.js
+++ notes-postgres/util/db.js
@@ -4,23 +4,6 @@ const { Umzug, SequelizeStorage } = require('umzug')
 
 const sequelize = new Sequelize(DATABASE_URL)
 
-const runMigrations = async () => {
-  const migrator = new Umzug({
-    migrations: {
-      glob: 'migrations/*.js',
-    },
-
-    storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
-    context: sequelize.getQueryInterface(),
-    logger: console,
-  })
-
-  const migrations = await migrator.up()
-  console.log('Migrations are up to date!', {
-    files: migrations.map((m) => m.name),
-  })
-}
-
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate()
@@ -30,6 +13,29 @@ const connectToDatabase = async () => {
     console.error(`Couldn't connect to Postgres`)
     return process.exit(1)
   }
+  return null
+}
+const migrationConf = {
+  migrations: {
+    glob: 'migrations/*.js',
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
 }
 
-module.exports = { connectToDatabase, sequelize }
+const runMigrations = async () => {
+  const migrator = new Umzug(migrationConf)
+
+  const migrations = await migrator.up()
+  console.log('Migrations are up to date!', {
+    files: migrations.map((m) => m.name),
+  })
+}
+
+const rollbackMigration = async () => {
+  await sequelize.authenticate()
+  const migrator = new Umzug(migrationConf)
+  await migrator.down()
+}
+module.exports = { connectToDatabase, sequelize, rollbackMigration }
